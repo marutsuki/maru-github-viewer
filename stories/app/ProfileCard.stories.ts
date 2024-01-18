469c9eae78297f2672767639e542d22b62fc3252
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-01-18 22:51:06 +1100

    Added storybook

---
+++ stories/app/ProfileCard.stories.ts
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ProfileCard from "../../app/components/ProfileCard";
+
+const meta: Meta<typeof ProfileCard> = {
+    component: ProfileCard,
+    title: "ProfileCard"
+}
+export default meta 
+
+export const Primary: StoryObj = {
+    args: {
+        title: "Name Name",
+        description: "",
+        imageUrl: "/identicon.png"
+    }
+}
+
+export const LongDescription: StoryObj = {
+    args: {
+        title: "Name Name",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consectetur eros lacus, vitae mattis nibh venenatis quis. Suspendisse potenti. Donec ultrices, libero quis laoreet maximus, enim velit tincidunt libero, sed feugiat urna nulla eu mi. Nunc vitae turpis quis erat fringilla tristique. Mauris condimentum nec odio a congue. Nulla eleifend dui iaculis risus auctor eleifend. Sed viverra arcu vitae mauris sagittis efficitur. Proin sed facilisis felis. Vestibulum fringilla nulla sagittis, ullamcorper erat sit amet, molestie erat. Vestibulum placerat, ligula eget porttitor sagittis, ipsum libero volutpat tortor, eu lacinia massa magna vel nisl. Maecenas aliquet nunc quam, ut vehicula velit ultrices eu.",
+        imageUrl: "/identicon.png"
+    }
+}
