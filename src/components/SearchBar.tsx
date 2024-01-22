27e143d4da5b3ee408fba3ae28cd1c2a036ccae6
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-01-22 22:11:21 +1100

    Made SearchBar more aesthetic

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { updateUserSearch } from "@/client/user/userSearchSlice";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { DoubleDash } from "./common/symbols";
 
 export default function SearchBar() {
     const dispatch = useDispatch();
@@ -10,10 +11,11 @@ export default function SearchBar() {
         dispatch(updateUserSearch(usernameFilter));
     }, [usernameFilter]);
 
-    return <div className="h-14 p-6 bg-primary flex justify-center">
-        <div className="relative">
-            <div className="h-8 w-80 absolute p-2 -translate-x-1/2 bg-thematic-gradient blur-md"></div>
-            <input className="h-8 w-80 absolute p-2 -translate-x-1/2 text-white bg-black bg-opacity-70 rounded-lg border border-white" placeholder="Search a GitHub user..." onChange={e => setUsernameFilter(e.target.value)}/>
+    return <div className="h-20 p-6 bg-neutral flex justify-center">
+        <div className="h-8 w-80 relative">
+            <DoubleDash className="absolute right-full -bottom-4"/>
+            <div className="h-full w-full absolute p-2 bg-thematic-gradient blur-md"></div>
+            <input className="h-full w-full absolute p-2 text-white bg-black bg-opacity-70 rounded-lg border border-white" placeholder="Search a GitHub user..." onChange={e => setUsernameFilter(e.target.value)}/>
         </div>
     </div>;
 }
